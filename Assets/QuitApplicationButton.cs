8f48803993e04331621c428ef49bf1640627c1b2
unknown <foxtrot117@hotmail.com>
2020-09-01 15:00:11 +0300

    executable added

---
+++ Assets/QuitApplicationButton.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitApplicationButton : MonoBehaviour {
+    public void Quit() {
+        Application.Quit();
+    }
+}
